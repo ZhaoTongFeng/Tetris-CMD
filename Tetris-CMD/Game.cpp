cdf2be782e84d47914dfa23f3e65f1a14cfce978
ZhaoTongFeng <tongfeng_zhao@163.com>
2020-09-07 16:52:31 +0800

    Finish

--- Tetris-CMD/Game.cpp
+++ Tetris-CMD/Game.cpp
@@ -1,10 +1,42 @@
-﻿#include "Game.h"
+﻿
+#include "Game.h"
+
+
+
+Game::~Game()
+{
+	for (int i = 0; i < mapSize.y; i++) {
+		delete[] map[i];
+	}
+	delete[] map;
+}
 
 bool Game::Initialize()
 {
+	//初始化地图
+	mapSize = Vector2(activitySize.x + 2 * (boxLength + wallWidth), activitySize.y + wallWidth + boxLength);
+	map = new unsigned char* [mapSize.y];
+	for (int i = 0; i < mapSize.y; i++) {
+		map[i] = new unsigned char[mapSize.x];
+		for (int j = 0; j < mapSize.x; j++) {
+			map[i][j] = 0;
+			if (j >= mapSize.x - boxLength - wallWidth || j <= boxLength + wallWidth - 1) {
+				map[i][j] = 3;
+			}
+			if (i == mapSize.y - 1) {
+				map[i][j] = 3;
+			}
+		}
+	}
+
+
 	tickCount = clock();
+
+	SpawnBlock();
+
 	return true;
 }
+
 
 void Game::Loop()
 {
@@ -12,55 +44,249 @@ void Game::Loop()
 		ProcessInput();
 		UpdateGame();
 		GenerateOutput();
+
 	}
 }
+
 
 void Game::Shutdown()
 {
 
 }
 
+
 void Game::ProcessInput()
 {
 	//处理输入
-	if (_kbhit()){
+	//有键盘输入返回1，没有返回0
+	p->dir_x = 0;
+	p->dir_y = 0;
+
+	if (_kbhit())
+	{
 		update = false;
 		int key = _getch();
 		if (key == 'a') {
-			//TEST
-			std::cout << "a" << std::endl;
+			p->dir_x -= 1;
+			//开启更新
+			update = true;
+		}
+		if (key == 'd') {
+			p->dir_x += 1;
+			update = true;
+		}
+		if (key == 's') {
+			p->dir_y += 1;
+			update = true;
+		}
+		if (key == 'w') {
+			rotate = true;
 			update = true;
 		}
 	}
 }
 
+
+
+
 void Game::UpdateGame()
 {
-	//锁定60帧率
-	while (clock() - tickCount < 16);
 	currentTick = clock();
-	//计算时间步长
-	float deltaTime = static_cast<float>((currentTick - tickCount)) / CLOCKS_PER_SEC;
-	tickCount = currentTick;
+	if (currentTick - tickCount > 1000) {
+		update = true;
+		p->dir_y = 1;
+		tickCount = currentTick;
+	}
 
-	//TEST
-	//update = true;
+	//更新游戏
+	if (!update) {
+		return;
+	}
+	//左右移动
 
-	//检测是否需要更新游戏
-	if (!update) { return; }
+	if (p->dir_x != 0) {
+		p->col += p->dir_x;
+		if (CheckArea(currentBlock)) {
+			p->col -= p->dir_x;
+		}
+
+	}
+
+	//向下移动
+	if (p->dir_y != 0) {
+		p->row += p->dir_y;
+		//检查是否会发生碰撞，如果会发生碰撞就不执行移动操作，固定方块生成新方块
+		if (CheckArea(currentBlock)) {
+			p->row -= p->dir_y;
+			FixBlocks();
+		}
+	}
+	if (rotate) {
+		Rotate();
+		rotate = false;
+	}
 }
 
+
+
+void Game::SpawnBlock()
+{
+	delete p;
+	p = new Point((int)(mapSize.x - boxLength) / 2, boxLength - 1);
+	srand((int)time(0));
+	//随机抽取形状
+	numIndex = rand() % typeLength;
+	//设置当前形状下标
+	blockIndex = sumNum[numIndex] + rand() % blockNum[numIndex];
+
+	SetCurrentBlock();
+	//地图中生成方块
+	for (int i = 0; i < boxLength; i++) {
+		for (int j = 0; j < boxLength; j++) {
+			map[p->row + i][p->col + j] = currentBlock[boxLength - i - 1][j];
+		}
+	}
+}
+
+void Game::Rotate()
+{
+
+	//旋转之后的下标
+	int temp = blockIndex;
+	blockIndex = sumNum[numIndex] + (((blockIndex - sumNum[numIndex]) % blockNum[numIndex]) + 1) % blockNum[numIndex];
+	unsigned char targetBlock[4][4];
+	for (int i = 0; i < boxLength; i++) {
+		for (int j = 0; j < boxLength; j++) {
+			targetBlock[i][j] = blocks[blockIndex][i][j];
+		}
+	}
+	if (CheckArea(targetBlock)) {
+		blockIndex = temp;
+	}
+	else {
+		SetCurrentBlock();
+	}
+
+}
+
+
+
+void Game::SetCurrentBlock()
+{
+	for (int i = 0; i < boxLength; i++) {
+		for (int y = 0; y < boxLength; y++) {
+			currentBlock[i][y] = blocks[blockIndex][i][y];
+		}
+	}
+}
+
+
+void Game::RemoveRow()
+{
+	for (int i = 0; i < mapSize.y; i++) {
+		int sum = 0;
+		for (int j = 0; j < mapSize.x; j++) {
+			if (map[i][j] == 2) {
+				sum++;
+			}
+		}
+
+		if (sum == activitySize.x) {
+			for (int n = i; n > 0; n--) {
+				for (int m = 0; m < mapSize.x; m++) {
+					map[n][m] = map[n - 1][m];
+				}
+			}
+		}
+	}
+
+}
+
+void Game::FixBlocks()
+{
+	for (int i = 0; i < boxLength; i++) {
+		for (int j = 0; j < boxLength; j++) {
+			int x = p->row - boxLength + 1 + i;
+			if (map[x][p->col + j] == 1) {
+				//检查游戏是否结束
+				if (x == 5) {
+					isRunning = false;
+					return;
+				}
+				map[x][p->col + j] = 2;
+			}
+		}
+	}
+	RemoveRow();
+	SpawnBlock();
+}
+
+void Game::SetArea()
+{
+	for (int i = 0; i < boxLength; i++) {
+		for (int j = 0; j < boxLength; j++) {
+			area[i][j] = map[p->row - boxLength + 1 + i][p->col + j];//小心数组越界
+		}
+	}
+}
+
+
+bool Game::CheckArea(unsigned char targetBlock[4][4])
+{
+	SetArea();
+	for (int i = 0; i < boxLength; i++) {
+		for (int j = 0; j < boxLength; j++) {
+			if ((area[i][j] == 3 || area[i][j] == 2) && targetBlock[i][j] == 1) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+
 void Game::GenerateOutput() {
-	//检测是否需要重新绘制
-	if (!update) { return; }
+	if (!update) {
+		return;
+	}
+	system("cls");
+	//先清除之前的移动方块
+	for (int i = 0; i < mapSize.y; i++) {
+		for (int j = 0; j < mapSize.x; j++) {
+			if (map[i][j] == 1) {
+				map[i][j] = 0;
+			}
+		}
+	}
+	//再把现在的把移动方块画上去
+	for (int i = 0; i < boxLength; i++) {
+		for (int j = 0; j < boxLength; j++) {
+			if (map[p->row - boxLength + 1 + i][p->col + j] == 0) {
+				map[p->row - boxLength + 1 + i][p->col + j] = currentBlock[i][j];
+			}
+		}
+	}
 
-	//输出图像
-	system("cls");//清屏
-
-	//TEST
-	std::cout << tickCount << std::endl;
-
-	//重置更新
+	//输出
+	for (int i = 0; i < mapSize.y; i++) {
+		for (int j = 0; j < mapSize.x; j++) {
+			if (map[i][j] == 0) {
+				printf("  ");
+			}
+			else if (map[i][j] == 1) {
+				printf("△");
+			}
+			else if (map[i][j] == 2) {
+				printf("□");
+			}
+			else if (map[i][j] == 3) {
+				printf("☆");
+			}
+		}
+		cout << endl;
+	}
+	//关闭更新
 	update = false;
 }
 
--- Tetris-CMD/Game.h
+++ Tetris-CMD/Game.h
@@ -1,38 +1,194 @@
-﻿
-#pragma once
-#include <iomanip>
-#include <windows.h>
+﻿#pragma once
+#include <time.h>
 #include <conio.h>
 #include <iostream>
-#include <stdio.h>
+
+using namespace std;
+
+
+
+
+//锚点
+//struct Point {
+//	Point(int a, int b) :x(a), y(b) {}
+//	int x, y;
+//};
+class Vector2 {
+public:
+	Vector2() { x = 0; y = 0; }
+	Vector2(int a, int b) :x(a), y(b) {}
+	int x, y;
+};
+
+class Point {
+public:
+	Point(int a, int b) :col(a), row(b) {}
+	int col, row;
+	int dir_x = 0, dir_y = 0;
+
+	Vector2 pos, dir;
+};
 
 class Game {
 public:
-	//初始化
+	~Game();
 	bool Initialize();
-	
-	//游戏循环
 	void Loop();
-	
-	//游戏结束
 	void Shutdown();
 
 private:
+
 	//按键输入
 	void ProcessInput();
-	
 	//更新游戏
 	void UpdateGame();
-	
 	//输出图像
 	void GenerateOutput();
 
-	//游戏是否结束
-	bool isRunning = true;
+
+
+	//设置锚点所占地图区域，用于碰撞检测
+	void SetArea();
+	//碰撞检测
+	bool CheckArea(unsigned char targetBlock[4][4]);
+
+	//生成方块
+	void SpawnBlock();
+
+	//旋转
+	void Rotate();
+
+	//根据blockIndex设置currentBlock
+	void SetCurrentBlock();
+
+	//消除行
+	void RemoveRow();
+	//固定方块
+	void FixBlocks();
+
+
+	//装载图形的矩阵宽度为4
+	const int boxLength = 4;
+	//当前方块在blocks中的下标
+	int blockIndex;
+	//方块的各种角度，总数11个
+	const unsigned char blocks[13][4][4] = {
+		{
+			{0,0,0,0},
+			{0,1,0,0},
+			{0,1,0,0},
+			{0,1,1,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{0,0,1,0},
+			{1,1,1,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,1,0},
+			{0,0,1,0},
+			{0,0,1,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,0},
+			{1,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{0,1,0,0},
+			{1,1,1,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,1,0},
+			{0,1,1,0},
+			{0,0,1,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,0},
+			{0,1,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,0,0,0},
+			{1,1,0,0},
+			{1,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{0,1,1,0},
+			{1,1,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,0,0},
+			{0,1,1,0},
+			{0,0,1,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{0,1,1,0},
+			{0,1,1,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,1}
+		},
+		{
+			{0,1,0,0},
+			{0,1,0,0},
+			{0,1,0,0},
+			{0,1,0,0}
+		}
+	};
+
+	//方块种类个数
+	const int typeLength = 5;
+	//当前形状下标
+	int numIndex;
+	//每种方块所占角度个数
+	const unsigned char blockNum[5] = { 4,4,2,1,2 };
+	//方块范围,0-3,4-7...
+	const unsigned char sumNum[5] = { 0,4,8,10,11 };
+
+	//当前方块
+	unsigned char currentBlock[4][4];
+	//方块所在的区域
+	unsigned char area[4][4];
+
+
+	//方块活动区域大小
+	Vector2 activitySize = Vector2(10, 20);
+	//整个画布大小
+	Vector2 mapSize;
+	//画布
+	unsigned char** map;
+
+	//围墙宽度
+	const int wallWidth = 1;
+
+
+	//锚点
+	Point* p;
+
+	//累计毫秒数
+	clock_t tickCount, currentTick;
 
 	//是否需要更新游戏
 	bool update = true;
+	//游戏是否结束
+	bool isRunning = true;
 
-	//累计毫秒数，
-	long tickCount, currentTick;
+	bool rotate;
 };
